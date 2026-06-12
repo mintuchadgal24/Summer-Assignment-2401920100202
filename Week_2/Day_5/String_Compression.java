398eca65365b9812b90433f4c51d028e5dd4e741
mintu_chadgal <chadgalmintu93@gmail.com>
2026-06-12 20:08:37 +0530

    Create String_Compression.java

---
+++ Week_2/Day_5/String_Compression.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int compress(char[] chars) {
+
+        int index = 0;
+        int i = 0;
+
+        while (i < chars.length) {
+
+            char current = chars[i];
+            int count = 0;
+            while (i < chars.length && chars[i] == current) {
+                count++;
+                i++;
+            }
+            chars[index++] = current;
+
+            if (count > 1) {
+                String s = String.valueOf(count);
+
+                for (char c : s.toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+        return index;
+    }
+}
