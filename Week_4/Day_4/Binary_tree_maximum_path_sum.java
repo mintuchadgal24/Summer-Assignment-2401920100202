b4c8cd3c64bc895240398a6af9e0a902b53096e7
mintu_chadgal <chadgalmintu93@gmail.com>
2026-06-26 12:31:10 +0530

    Create Binary_tree_maximum_path_sum.java

---
+++ Week_4/Day_4/Binary_tree_maximum_path_sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    int maxSum = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+    private int maxGain(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = Math.max(0, maxGain(root.left));
+        int right = Math.max(0, maxGain(root.right));
+        maxSum = Math.max(maxSum, root.val + left + right);
+        return root.val + Math.max(left, right);
+    }
+}
